6606cdd26635ea5548be6cfbddc5e526aa9c72bb
blumj808 <blumenfeld.jake@gmail.com>
2023-11-18 17:38:11 +0000

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
 
     render({:template => "users_templates/index"})
   end
+
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({:username => input_username})
+   
+   @the_user = matching_usernames.first
+
+    render({:template => "users_templates/show"})
+   end
 end
